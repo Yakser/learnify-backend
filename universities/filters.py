c68973f48d44f7e78b84afa013e977d4188d2d6d
Yakser <iaksanov2044@mail.ru>
2023-01-07 14:30:31 +0400

    Added filtering by multiple tags

--- universities/filters.py
+++ universities/filters.py
@@ -3,6 +3,15 @@ from django_filters import rest_framework as filters
 from universities.models import University, Specialization, Department
 
 
+def filter_by_tags(queryset, name, value):
+    tags = value
+    if tags:
+        tags = tags.split(",")
+        queryset = queryset.filter(tags__name__in=tags).distinct()
+
+    return queryset
+
+
 class UniversityFilter(filters.FilterSet):
     """
     Filter for Universities
@@ -11,19 +20,25 @@ class UniversityFilter(filters.FilterSet):
     Parameters to filter by:
         name (str)
         city (str)
+        tags (str)
     Examples:
         ?name=test equals to .filter(name='test')
+        ?tags=tag1,tag2 equals to .filter(tags__name__in=tags).distinct()
         ?name__contains=yawning equals to .filter(name__contains='yawning')
     """
 
     name__contains = filters.Filter(field_name="name", lookup_expr="contains")
+    tags = filters.CharFilter(
+        field_name="tags__name",
+        method=filter_by_tags,
+    )
 
     class Meta:
         model = University
         fields = (
             "name",
             "city",
-            "tags__name",
+            "tags",
         )
 
 
@@ -40,12 +55,16 @@ class SpecializationFilter(filters.FilterSet):
     """
 
     name__contains = filters.Filter(field_name="name", lookup_expr="contains")
+    tags = filters.CharFilter(
+        field_name="tags__name",
+        method=filter_by_tags,
+    )
 
     class Meta:
         model = Specialization
         fields = (
             "name",
-            "tags__name",
+            "tags",
         )
 
 
@@ -62,10 +81,14 @@ class DepartmentFilter(filters.FilterSet):
     """
 
     name__contains = filters.Filter(field_name="name", lookup_expr="contains")
+    tags = filters.CharFilter(
+        field_name="tags__name",
+        method=filter_by_tags,
+    )
 
     class Meta:
         model = Department
         fields = (
             "name",
-            "tags__name",
+            "tags",
         )
