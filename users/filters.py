189d488415b4b1fa03146a7f255c445157f74bbd
Yakser <iaksanov2044@mail.ru>
2023-10-08 17:47:10 +0300

    add filter by fullname for users

--- users/filters.py
+++ users/filters.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 User = get_user_model()
@@ -15,11 +16,30 @@ class UserFilter(filters.FilterSet):
         ?username__contains=yawning equals to .filter(username__contains='yawning')
     """
 
+    @classmethod
+    def filter_by_fullname(cls, queryset, name, value):
+        words = value.split()
+        first_word = words[0]
+        if len(words) >= 2:
+            # if there are more than 2 words, we assume that the first two are first_name and last_name
+            first_word, second_word = words[0], words[1]
+            # we search for both first_name and last_name in both orders
+            return queryset.filter(
+                Q(first_name__icontains=first_word)
+                | Q(last_name__icontains=second_word)
+                | Q(first_name__icontains=second_word)
+                | Q(last_name__icontains=first_word)
+            )
+        return queryset.filter(
+            Q(first_name__icontains=first_word) | Q(last_name__icontains=first_word)
+        )
+
     username__contains = filters.Filter(field_name="username", lookup_expr="contains")
     first_name__contains = filters.Filter(
         field_name="first_name", lookup_expr="contains"
     )
     last_name__contains = filters.Filter(field_name="last_name", lookup_expr="contains")
+    fullname = filters.CharFilter(method="filter_by_fullname")
 
     class Meta:
         model = User
